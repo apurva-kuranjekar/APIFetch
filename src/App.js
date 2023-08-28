985c4b35ac469464bb7ecd76a27d7b965fb1400b
apurva-kuranjekar <apurwakuranjekar100@gmail.com>
2023-08-28 16:00:46 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    fetch('https://api.publicapis.org/categories')
+      .then(response => response.json())
+      .then(data => {
+        setCategories(data.categories);
+      })
+      .catch(error => {
+        console.error('Error fetching categories:', error);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row">
+        {categories.map(category => (
+          <div className="col-md-4" key={category}>
+            <div className="card" style={{ width: '14rem' }}>
+              <div className="card-body">
+                <h5 className="card-title">{category}</h5>
+                <p className="card-text">
+                {category}
+                </p>
+                <h6 className="card-category">{category}</h6>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
